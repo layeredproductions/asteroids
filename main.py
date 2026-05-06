cf5faa33a227de1db04704c0e0d4c082b2523d5a
paul <126406614+layeredproductions@users.noreply.github.com>
2026-05-06 10:12:50 -0400

    added player shape and basic player render

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from  constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
@@ -16,6 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
